2c03b2ca4b2c64c4f4cfb8237debd822ab319e90
BIN IM <mandarino.viva.21@gmail.com>
2022-01-23 20:03:23 +0900

    추천 로드맵 UI 수정

--- src/pages/roadmap/RecommendPage.tsx
+++ src/pages/roadmap/RecommendPage.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Layout, Button, Tag } from "antd";
+import { Row, Col, Card, Button, Tag } from "antd";
+import { HeartTwoTone, ShareAltOutlined } from '@ant-design/icons';
 import { observer } from "mobx-react";
 import store from "./store/store";
 import "./RecommendPage.css";
-import { devStackList, descriptionList } from './store/data';
+import { devStackList, descriptionList } from "./store/data";
 import { DevStack } from "./store/types";
 
 const RecommendPage: React.FC = observer(() => {
@@ -15,27 +16,48 @@ const RecommendPage: React.FC = observer(() => {
     setRecommendList();
   }, []);
 
-  const tag: DevStack | undefined = devStackList.find(item => item.id === devStack);
+  const tag: DevStack | undefined = devStackList.find(
+    (item) => item.id === devStack
+  );
 
   const detailViewClicked = (id: number) => {
-	navigate(`/roadmap/${id}`);
-  }
+    navigate(`/roadmap/${id}`);
+  };
 
   return (
     <div className="container">
-      <Tag className="tag" color={tag?.color}>{tag?.title}</Tag>
+      <Tag className="tag" color={tag?.color}>
+        {tag?.title}
+      </Tag>
       <h1>나에게 맞는 추천 로드맵은 어느것 일까요?</h1>
       <div>
-        {recommendList.map((item) => (
-          <div className="card" key={item.id}>
-			  <p><span className="user-name">{item.userName}</span> 님의 {descriptionList[item.description]} 로드맵</p>
-			  <div className="count-box"> 
-				  <p className="count">{item.favorite}</p>
-				  <p className="count">{item.share}</p>
-			  </div>
-			  <Button onClick={detailViewClicked.bind(this, item.id)}>보러 가기</Button>
-		  </div>
-        ))}
+        <Row gutter={16}>
+          {recommendList.map((item) => {
+            const title = [
+              <span key={item.id} className="user-name">{item.userName}</span>,
+              " 님의 ",
+              descriptionList[item.description],
+              " 로드맵",
+            ];
+            return (
+                <Col key={item.id} span={12} className="card-container">
+                <Card title={title}
+                actions={[
+                    
+                ]}
+                >
+                  <div className="count-box">
+                    <p className="count"> <HeartTwoTone twoToneColor="#eb2f96"/>{item.favorite}</p>
+                    <p className="count"><ShareAltOutlined />{item.share}</p>
+                  </div>
+                  <Button onClick={detailViewClicked.bind(this, item.id)}>
+                    보러 가기
+                  </Button>
+                </Card>
+                </Col>
+            );
+          })}
+        </Row>
       </div>
     </div>
   );
