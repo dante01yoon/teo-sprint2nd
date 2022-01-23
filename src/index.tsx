f3935a2da70a0e25ccbb949dea9d6b391c8e4b87
설아아빠 <dhrod0325@naver.com>
2022-01-23 18:01:27 +0900

    그래프 샘플 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PersonalPage from './pages/roadmap/PersonalPage';
-import RecommendPage from './pages/roadmap/RecommendPage';
-import RoadmapPage from './pages/roadmap/RoadmapPage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import PersonalPage from "./pages/roadmap/PersonalPage";
+import RecommendPage from "./pages/roadmap/RecommendPage";
+import RoadmapPage from "./pages/roadmap/RoadmapPage";
+import GraphSample from "./pages/roadmap/GraphSample";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}/>
-            <Route path="/personal" element={<PersonalPage/>}/>
-            <Route path="/recommend" element={<RecommendPage/>}/>
-            <Route path="/roadmap" element={<RoadmapPage/>}/>
-            
-        </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/recommend" element={<RecommendPage />} />
+        <Route path="/roadmap" element={<RoadmapPage />} />
+        <Route path="/graphSample" element={<GraphSample />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
