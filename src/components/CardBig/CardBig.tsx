92bbcbfa50cbaf9a3c9dea1168826185cdadc80b
dev-jun <42564107+jun094@users.noreply.github.com>
2022-01-23 22:50:25 +0900

    fix(Statics): 코드리뷰 사항 반영

--- src/components/CardBig/CardBig.tsx
+++ src/components/CardBig/CardBig.tsx
@@ -6,15 +6,10 @@ import style from './CardBig.module.css';
 type CardBigProps = {
 	children: React.ReactNode;
 	className?: string;
-	key?: string | number;
 };
 
-function CardBig({ key, className, children }: CardBigProps) {
-	return (
-		<div key={key} className={cx(style.CardBig, className)}>
-			{children}
-		</div>
-	);
+function CardBig({ className, children }: CardBigProps) {
+	return <div className={cx(style.CardBig, className)}>{children}</div>;
 }
 
 export default CardBig;
